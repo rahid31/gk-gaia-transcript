86e190db00de0b5ca9e4f8cf19cd8698b7f76905
Alvin Dihar <dihar.alvin31@gmail.com>
2025-04-22 09:07:41 +0700

    Update: NEXA, company logo, data

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ JSON_FILE_PATH_2 = "https://raw.githubusercontent.com/rahid31/gk-gaia-transcript
 # Avatar Icon
 user_url = "data/image/user-square-1024.webp"
 avatar_url = "data/image/Logo GAIA.png"
-page_icon = "data/image/Logo Gokampus.png"
+page_icon = "data/image/Icon Apps - @192.png"
 
 # Function to fetch JSON from GitHub
 def fetch_json(url):
@@ -107,7 +107,7 @@ if session_id:
 
     # Footer
     st.divider()
-    st.image("https://gk-analyst.s3.ap-southeast-1.amazonaws.com/metabase-dashboard/gokampus_logo.svg", width=100)
+    st.image(r"data\image\LX Primary, Color - @256.png", width=120)
 
 else:
     st.error("No session_id provided in the URL.")
